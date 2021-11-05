033ddd4b7432aecd02246b30ef8474635f0891a0
Denis <45011033+feezeee@users.noreply.github.com>
2021-11-05 13:54:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+
+            Color myColorPurpule = new Color
+            {
+                ColorName = "Фиолетовый"
+            };
+
+            myColorPurpule.Colors.AddRange(
+                new Color[]
+                {
+                    new Color{ColorName = "Голубой"}
+                });
+
+
+
+            Color myColorBlue = new Color
+            {
+                ColorName = "Синий"
+            };
+
+            myColorBlue.Colors.AddRange(
+                new Color[]
+                {
+                    new Color{ColorName = "Оранжевый"},
+                    new Color{ColorName = "Желтый"}
+                });
+
+            Color myColor = new Color
+            {
+                ColorName = "Зеленый"
+            };
+
+            myColor.Colors.AddRange(
+                new Color[]
+                {
+                    myColorBlue,
+                    new Color { ColorName = "Красный" },
+                    myColorPurpule
+                });
+
+            var peremen = myColor;
+        }
+    }
+}
